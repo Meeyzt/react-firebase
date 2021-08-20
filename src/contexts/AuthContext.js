12abea13197904d6ad8352c59327952fc1126d03
Mehmet Yılmaz <64976979+Meeyzt@users.noreply.github.com>
2021-08-20 13:49:59 +0300

    Create AuthContext

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,24 @@
+import React, { useContext, useEffect, useState } from "react";
+import { auth } from "../firebase/firebase";
+
+export const AuthContext = React.createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [currentUser, setCurrentUser] = useState(null);
+  useEffect(() => {
+    auth().onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+  }, []);
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  return (
+    <AuthContext.Provider value={{ currentUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+export const useAuth = () => useContext(AuthContext);
