a163cec9ac794aaa4b5c5ae234ffc48f500f8957
Mehmet Yılmaz <64976979+Meeyzt@users.noreply.github.com>
2021-08-20 14:00:04 +0300

    Login confirmation

--- src/App.js
+++ src/App.js
@@ -3,37 +3,44 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Login from "./components/Auth/Login";
 import Home from "./components/Home";
 import Register from "./components/Auth/Register";
-import { AuthProvider } from "./contexts/AuthContext";
+import { useAuth } from "./contexts/AuthContext";
 import Dashboard from "./components/Dashboard";
 
 function App() {
+  const { currentUser } = useAuth();
   return (
     <>
-      <AuthProvider>
-        <Router>
-          <div>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/register">Register</Link>
-                </li>
-                <li>
-                  <Link to="/Login">Login</Link>
-                </li>
-              </ul>
-            </nav>
-            <Switch>
-              <Route path="/login" component={Login} />
-              <Route path="/register" component={Register} />
-              <Route path="/Dashboard" component={Dashboard} />
-              <Route path="/" component={Home} />
-            </Switch>
-          </div>
-        </Router>
-      </AuthProvider>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              {currentUser ? (
+                <p>
+                  You are Logged - <Link to="/dashboard">View Dashboard</Link>
+                </p>
+              ) : (
+                <p>
+                  <li>
+                    <Link to="/register">Register</Link>
+                  </li>
+                  <li>
+                    <Link to="/Login">Login</Link>
+                  </li>
+                </p>
+              )}
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </div>
+      </Router>
     </>
   );
 }
