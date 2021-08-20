7e41b003a6ae4b112fd0602b9751416866231902
Mehmet Yılmaz <64976979+Meeyzt@users.noreply.github.com>
2021-08-20 13:50:50 +0300

    Add AuthProvider

--- src/App.js
+++ src/App.js
@@ -3,32 +3,35 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Login from "./components/Auth/Login";
 import Home from "./components/Home";
 import Register from "./components/Auth/Register";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
     <>
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/register">Register</Link>
-              </li>
-              <li>
-                <Link to="/Login">Login</Link>
-              </li>
-            </ul>
-          </nav>
-          <Switch>
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-            <Route path="/" component={Home} />
-          </Switch>
-        </div>
-      </Router>
+      <AuthProvider>
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/register">Register</Link>
+                </li>
+                <li>
+                  <Link to="/Login">Login</Link>
+                </li>
+              </ul>
+            </nav>
+            <Switch>
+              <Route path="/login" component={Login} />
+              <Route path="/register" component={Register} />
+              <Route path="/" component={Home} />
+            </Switch>
+          </div>
+        </Router>
+      </AuthProvider>
     </>
   );
 }
