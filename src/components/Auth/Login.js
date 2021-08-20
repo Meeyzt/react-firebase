0e7f645d309c8f1c1d621f1835babbf8b5ab4fb0
Mehmet Yılmaz <64976979+Meeyzt@users.noreply.github.com>
2021-08-20 14:00:31 +0300

    Edit Login Component

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { auth } from "../../firebase/firebase";
+import { useAuth } from "../../contexts/AuthContext";
+import { Redirect } from "react-router-dom";
 
 function Login() {
-  return <div>Login</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { email, password } = e.target.elements;
+    try {
+      auth.signInWithEmailAndPassword(email.value, password.value);
+      console.log("Login: Logged IN");
+    } catch (error) {
+      console.log("Login: ", error);
+    }
+  };
+  const { currentUser } = useAuth();
+  if (currentUser) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  return (
+    <>
+      <h1>Log In</h1>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <label for="email">Email</label>
+        <input type="email" name="email" placeholder="Email" />
+        <label for="password">Password</label>
+        <input type="password" name="password" placeholder="Password" />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
 }
 
 export default Login;
