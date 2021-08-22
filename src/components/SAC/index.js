bff081638545336d1b5fc09f95b790df983e7465
Mehmet Yılmaz <64976979+Meeyzt@users.noreply.github.com>
2021-08-22 22:57:26 +0300

    Add "Space Around Components" component

---
+++ src/components/SAC/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Index() {
+  // Space Around Components
+  return (
+    <>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </>
+  );
+}
+
+export default Index;
